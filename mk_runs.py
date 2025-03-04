0aaf5a78c0871af6020c533f4c14a3af80a4b84d
Peter Teuben <teuben@gmail.com>
2025-03-03 21:35:23 -0500

    bank=1

--- mk_runs.py
+++ mk_runs.py
@@ -27,8 +27,8 @@ pars2['Taurus_Edge']   = "pix_list=0,2 bank=0"
 
 #        common parameters per source on subsequent runs (run1c, run2c), e.g. bank=1 for WARES
 pars3 = {}
-pars3['Taurus_Peak']   = "srdp=1 admit=0"
-pars3['Taurus_Edge']   = "srdp=1 admit=0"
+pars3['Taurus_Peak']   = "pix_list=1,3 bank=1"
+pars3['Taurus_Edge']   = "pix_list=1,3 bank=1"
 
 if __name__ == '__main__':    
     runs.mk_runs(project, on, pars1, pars2, pars3, sys.argv)
